b1531110b48f2fec18b24efcb0455e6774718b57
plumest <chayathon.k@ku.th>
2019-07-12 21:53:21 +0700

    merge

--- Software/Back-End/getData.js
+++ Software/Back-End/getData.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function getHeartList(index=0, heartrate=0) {
     return [index, heartrate]
 }
@@ -13,3 +15,24 @@ setInterval(() => {
     let sec = new Date().getSeconds();
     document.getElementById('test').innerHTML = sec;
 }, 1000)
+
+let data_tested = {
+    'abcabc1234': {
+        'username': 'abcabc1234',
+        'password': '1234',
+        'firstname': 'abcabc',
+        'lastname': '1234',
+        'email': 'abcabc1234@hotmail.com',
+        'age': '20',
+        'sex': 'male',
+        'blood': 'B',
+        'allergic': '',
+        'phobia': 'hemophobia',
+        'other': ''
+    }
+}
+
+fs.appendFile('abcabc1234', data_tested, (err, fd) => {
+    if (err) throw err;
+    console.log('sucess')
+})
